fb2086a85c323ec02d415b884c74c50add7d2ba4
cecinuga <cecinuga.tdb@gmail.com>
2025-01-09 23:54:49 +0100

    recursive_file_visit callback

--- callbacks/recursive_file_visit.py
+++ callbacks/recursive_file_visit.py
@@ -14,7 +14,9 @@ def recursive_visit(path: str, output: list[str]=[]) -> list[str]:
     if os.path.isfile(path): 
         output.append(path)
     else:
-        for item in listdir(path).reverse():
+        list_dir = listdir(path)
+        list_dir.reverse()
+        for item in list_dir:
             if item not in exclude_item:
                 recursive_visit(f'{path}/{item}')
     
