c03e0137f028e33af2efe2dd6e99d1f339ee43ce
cecinuga <cecinuga.tdb@gmail.com>
2025-01-09 15:53:50 +0100

    README

---
+++ callbacks/recursive_file_visit.py
@@ -0,0 +1,30 @@
+import os
+from os import listdir
+
+input_dir = execution_callback_dir # type: ignore
+exclude_item = exclude #type: ignore
+
+def from_path_to_filename(path: str) -> str:
+    return path.split('/')[-1]
+
+def from_path_to_repopath(path: str) -> str:
+    return '/'.join(path.split('/')[-3:]) #TODO: make path depth dynamic
+
+def recursive_visit(path: str, output: list[str]=[]) -> list[str]:
+    if os.path.isfile(path): 
+        output.append(path)
+    else:
+        for item in listdir(path):
+            if item not in exclude_item:
+                recursive_visit(f'{path}/{item}')
+    
+    return output
+
+if __name__ == '__main__':
+    file_list = recursive_visit(input_dir)
+    result = [ {  
+                'name': from_path_to_filename(file).replace('_', '-'), 
+                'path': from_path_to_repopath(file),
+                'difficulty': from_path_to_repopath(file).split('/')[0],
+                'language': from_path_to_repopath(file).split('/')[1]
+            } for file in file_list ]
---
+++ markplate.py
@@ -0,0 +1,64 @@
+import os
+import sys
+from os import listdir
+from markdownify import markdownify as md
+from jinja2 import Environment, FileSystemLoader
+
+if __name__ == '__main__':
+    templateLoader = FileSystemLoader(searchpath='./templates/')
+    templateEnv = Environment(loader=templateLoader)
+    template_var_key = [ ]
+    template_var_value = [ ]
+
+    argv = []
+    for i, arg in enumerate(sys.argv[1:]):
+        if arg[0:2] != '--' and arg[0] == '-':
+            template_var_key.append(arg)
+            template_var_value.append(sys.argv[i+1])
+        else: 
+            argv.append(arg)
+
+    #print(argv)
+    #print(template_var_key)
+    #print(template_var_value)
+
+    for arg in [ '--temp', '--out', '--out-file', '--cb', '--ex-cb-dir' ]:
+        if arg not in argv:
+            raise SyntaxError(f'mandatory argument not submittet: {arg} ')
+
+    for key, value in zip(argv[::2], argv[1::2]):
+        print(key, value)
+        match key:
+            case '--temp':
+                template = templateEnv.get_template(value)
+            case '--out':
+                outDir = value
+            case '--out-file':
+                outFile = value
+            case '--cb':
+                callback_file = value
+            case '--ex-cb-dir':
+                execution_callback_dir = value
+            case '--exclude':
+                exclude = value
+            case _:
+                raise SyntaxError(f'argument not valid: {key}')
+    print('----------------------------------')
+    loc = vars()
+    callback_result = exec(open(callback_file).read(), globals(), loc)
+    result = loc['result']
+    #print(result)
+
+    render = template.render(
+        username='cecinuga',
+        leetcodes=result
+    )
+    markdown = md(render)
+
+    output = f'{os.path.abspath(outDir)}/{outFile}'
+    open(output, 'rb').close()
+    
+    with open(output, 'w') as file:
+        file.write(markdown)
+        file.close()
+    
